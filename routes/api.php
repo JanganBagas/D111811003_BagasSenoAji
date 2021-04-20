ff15ef199f005b860d27af9b00b50685ef8336ae
Bagas Seno Aji <bagassenoaji8@gmail.comgit config --global user.email bagassenoaji8@gmail.com>
2021-04-20 22:55:49 +0700

    UTS_BagasSenoAJi

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::resource('/D111811003_admin', D111811003_adminController::class);
+Route::resource('/D111811003_news', D111811003_newsController::class);
